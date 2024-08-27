41b54e4a20e9b00fb862c2089c45f4eb06fc75d4
Jakob Duus Terkelsen <36190025+jduust@users.noreply.github.com>
2024-08-27 10:12:54 +0200

    Update process.py
    Allows queue_element to be sent to process, but only optional so linear process will also work without queue_element.

--- robot_framework/process.py
+++ robot_framework/process.py
@@ -1,8 +1,9 @@
 """This module contains the main process of the robot."""
 
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
+from OpenOrchestrator.database.queues import QueueElement
 
 
-def process(orchestrator_connection: OrchestratorConnection) -> None:
+def process(orchestrator_connection: OrchestratorConnection, queue_element: QueueElement | None = None) -> None:
     """Do the primary process of the robot."""
     orchestrator_connection.log_trace("Running process.")
