2db46b5c48789eaef37738c83ace5b751b921ad0
Jakob Duus Terkelsen <36190025+jduust@users.noreply.github.com>
2025-02-12 12:44:17 +0100

    Update config.py
    added queue_attempts to config

--- robot_framework/config.py
+++ robot_framework/config.py
@@ -3,6 +3,9 @@
 # The number of times the robot retries on an error before terminating.
 MAX_RETRY_COUNT = 3
 
+# Number of attempts per queue_element (1 is no retry, 2 is 2 total attempts and so on)
+QUEUE_ATTEMPTS = 1
+
 # Whether the robot should be marked as failed if MAX_RETRY_COUNT is reached.
 FAIL_ROBOT_ON_TOO_MANY_ERRORS = True
 
